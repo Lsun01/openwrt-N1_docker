211c7455f013104e2d24345168019e5ce83d7151
Lsun01 <xroot009@icloud.com>
2020-02-10 15:33:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,10 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/Lienol/openwrt-package package/openwrt-package
+#now dir is openwrt
+./scripts/feeds update -a
+./scripts/feeds install -a
