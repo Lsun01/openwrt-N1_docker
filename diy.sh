e655111ae26fa4602561e6dbd23a845fd1df0fd2
Lsun01 <xroot009@icloud.com>
2020-04-30 11:38:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,6 +11,9 @@ git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/Lienol/openwrt-package package/openwrt-package
 #now dir is openwrt
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 sudo apt-get install gcc-multilib g++-multilib
