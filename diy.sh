df41e06028dba0d4153ff951e0e9f0f3ec1da587
Lsun01 <xroot009@icloud.com>
2020-05-07 10:01:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,6 +8,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/vernesong/OpenClash package/luci-app-OpenClash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/Lienol/openwrt-package package/openwrt-package
 #now dir is openwrt
